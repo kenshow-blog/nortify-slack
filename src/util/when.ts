a576d759d740741cf0944a095d2dff4d1dde1422
kenshow-blog <kensho02131@gmail.com>
2023-02-25 16:49:37 +0900

    微修正

--- src/util/when.ts
+++ src/util/when.ts
@@ -1,11 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 type ChainedWhen<T, R> = {
   on: <A>(pred: (v: T) => boolean, fn: () => A) => ChainedWhen<T, R | A>;
   otherwise: <A>(fn: () => A) => R | A;
 };
 
 const match = <T, R>(val: any): ChainedWhen<T, R> => ({
-  on: <A>(pred: (v: T) => boolean, fn: () => A) => match<T, R | A>(val),
-  otherwise: <A>(fn: () => A): A | R => val,
+  on: <A>(_pred: (v: T) => boolean, _fn: () => A) => match<T, R | A>(val),
+  otherwise: <A>(_fn: () => A): A | R => val,
 });
 
 const chain = <T, R>(val: T): ChainedWhen<T, R> => ({
