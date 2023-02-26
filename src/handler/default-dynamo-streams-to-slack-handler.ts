c2ad5d45d3594da2e7df7c509fbef492dc924a76
kenshow-blog <kensho02131@gmail.com>
2023-02-26 16:30:42 +0900

    スリープ処理解除

--- src/handler/default-dynamo-streams-to-slack-handler.ts
+++ src/handler/default-dynamo-streams-to-slack-handler.ts
@@ -16,23 +16,19 @@ export class DefaultDynamoStreamsToSlackHandler {
   ) {}
   async execute(event: DynamoDBStreamEvent): Promise<void> {
     console.log("通知処理を開始します。");
-    event.Records.forEach(async (record) => {
+    event.Records.forEach((record) => {
       if (record.eventName !== "INSERT") return;
       console.log("Stream record: ", JSON.stringify(record, null, 2));
 
       const message = this.messageService.createSlackMessage(record);
-      let res: AxiosResponse<any, any>;
       try {
-        res = await this.slack.nortice(message);
-        console.log(res);
+        this.slack.nortice(message);
       } catch (e) {
         console.error("通知処理に失敗しました。", e);
         throw new Error(e);
       }
     });
-    await this._sleep(10000);
-    console.log("10秒待ったのち通知処理が完了しました。");
+    console.log("通知処理が完了しました。");
     return;
   }
-  _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 }
